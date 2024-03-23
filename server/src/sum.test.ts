50e9b969ae0a796e3e8f60678d7e0a592975c299
Arjan Dhakal <dhakal.arjan@gmail.com>
2024-03-23 10:02:21 +0545

    moving gitignore file to root

---
+++ server/src/sum.test.ts
@@ -0,0 +1,5 @@
+import { sum } from "./sum";
+
+test("sum works", () => {
+  expect(sum([1, 2, 3])).toBe(6);
+});
---
+++ server/src/sum.ts
@@ -0,0 +1,7 @@
+export function sum(arr: Array<number>) {
+  let sum = 0;
+  for (let n of arr) {
+    sum += n;
+  }
+  return sum;
+}
