87d661d44c2e8fd6d1e66ed7565ef71fbf7c98db
Arjan Dhakal <dhakal.arjan@gmail.com>
2024-03-23 16:52:15 +0545

    /api/product/v1 route and test added

--- server/src/product/routes.ts
+++ server/src/product/routes.ts
@@ -1,13 +1,15 @@
 import { Router, Request, Response } from "express";
 import { asyncWrapper } from "../utils/async";
+import { fetchproducts } from "./db";
 
 export const productRouter = Router();
 
 productRouter.get(
   "/",
   asyncWrapper(async (req: Request, res: Response) => {
+    const products = await fetchproducts();
     res
       .status(200)
-      .json({ products: [], message: "fetched products succesfully" });
-  }),
+      .json({ data: products, message: "fetched products succesfully" });
+  })
 );
